a1791e398561d4449cccf7feeb9b26a59ea8bc2e
Roy QIU <karoyqiu@gmail.com>
2023-09-12 16:04:45 +0800

    添加返回按钮。

--- src/DanmakuPage.tsx
+++ src/DanmakuPage.tsx
@@ -28,8 +28,10 @@ export default function DanmakuPage() {
   return (
     <div
       className="flex flex-col-reverse gap-2 p-2 overflow-y-auto"
-      onMouseDown={async () => {
-        await appWindow.startDragging();
+      onMouseDown={async (event) => {
+        if (event.currentTarget === event.target) {
+          await appWindow.startDragging();
+        }
       }}
       style={{ width: '100%', height: '100%' }}
     >
@@ -39,6 +41,31 @@ export default function DanmakuPage() {
           <div className="chat-bubble text-sky-300">{item.message}</div>
         </div>
       ))}
+      <button
+        className="btn btn-circle btn-ghost absolute right-6 top-2"
+        onClick={() => roomid.set(0)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            d="M14 7.63636L14 4.5C14 4.22386 13.7761 4 13.5 4L4.5 4C4.22386 4 4 4.22386 4 4.5L4 19.5C4 19.7761 4.22386 20 4.5 20L13.5 20C13.7761 20 14 19.7761 14 19.5L14 16.3636"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M10 12L21 12M21 12L18.0004 8.5M21 12L18 15.5"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
