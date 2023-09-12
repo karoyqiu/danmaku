2a8ff7818205dda68804c3d5c3ac57ac64cfe3b4
Roy QIU <karoyqiu@gmail.com>
2023-09-12 16:35:48 +0800

    设置 CSP。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,45 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod error;
-
-use error::Result;
-use tauri::{AppHandle, Manager};
-use window_vibrancy::{apply_acrylic, clear_acrylic};
-
-#[tauri::command]
-fn enable_background(app: AppHandle) -> Result<()> {
-  log::info!("Enable background");
-  let window = app.get_window("main").unwrap();
-  let result = apply_acrylic(&window, Some((18, 18, 18, 125)));
-
-  if let Err(error) = result {
-    log::error!("{}", error);
-  }
-
-  Ok(())
-}
-
-#[tauri::command]
-fn disable_background(app: AppHandle) -> Result<()> {
-  log::info!("Disable background");
-  let window = app.get_window("main").unwrap();
-  let result = clear_acrylic(&window);
-
-  if let Err(error) = result {
-    log::error!("{}", error);
-  }
-
-  Ok(())
-}
-
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_window_state::Builder::default().build())
-    .invoke_handler(tauri::generate_handler![
-      enable_background,
-      disable_background
-    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
+document.addEventListener('contextmenu', (event) => {
+  if (!event.target || !('tagName' in event.target) || event.target.tagName !== 'INPUT') {
+    event.preventDefault();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
