06479276de31decd54a2c184da1a85aa048868ca
Roy QIU <karoyqiu@gmail.com>
2023-09-13 08:55:43 +0800

    记录之前输入的 roomid。

--- src/SignInPage.tsx
+++ src/SignInPage.tsx
@@ -6,32 +6,31 @@ export default function SignInPage() {
   const [input, setInput] = React.useState('');
 
   return (
-    <div
+    <form
       className="flex flex-col gap-4 p-8 items-center justify-center w-full h-full"
       onMouseDown={async (event) => {
         if (event.currentTarget === event.target) {
           await appWindow.startDragging();
         }
       }}
+      onSubmit={(event) => {
+        event.preventDefault();
+        roomid.set(parseInt(input, 10));
+      }}
     >
       <input
-        type="text"
         className="input input-bordered w-full"
+        name="roomid"
+        type="text"
         autoComplete="on"
         placeholder="Input room ID"
         autoFocus
         value={input}
         onChange={(event) => setInput(event.target.value)}
       />
-      <button
-        className="btn btn-primary"
-        disabled={input.length === 0}
-        onClick={() => {
-          roomid.set(parseInt(input, 10));
-        }}
-      >
+      <button type="submit" className="btn btn-primary" disabled={input.length === 0}>
         Enter
       </button>
-    </div>
+    </form>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
-document.addEventListener('contextmenu', (event) => {
-  if (!event.target || !('tagName' in event.target) || event.target.tagName !== 'INPUT') {
-    event.preventDefault();
-  }
-});
+if (import.meta.env.PROD) {
+  document.addEventListener('contextmenu', (event) => {
+    if (!event.target || !('tagName' in event.target) || event.target.tagName !== 'INPUT') {
+      event.preventDefault();
+    }
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
